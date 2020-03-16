352de4f471f63a84db2822e96ee835afeb6a512e
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2020-03-16 15:35:11 -0300

    exposing

--- projects/oang/src/public-api.ts
+++ projects/oang/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of oang
  */
 
-export * from './lib/oang.module';
 export * from './lib/components/field.component';
+export * from './lib/oang-engine';
+export * from './lib/oang.module';
